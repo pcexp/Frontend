c195672a22332dac52a458d79539fdf6c81ed25e
Oguzhan95275 <oguzhangurcan99@gmail.com>
2024-07-29 20:50:30 +0300

    update 290724

--- project.js
+++ project.js
@@ -1,4 +1,12 @@
 function changeVisibility() {
     let register_page = document.getElementById("id_register_page");
-    register_page.className = "register_page";
+    switch(register_page.className) {
+        case "register_page":
+            register_page.className = "register_page_disabled";
+            break;
+        case "register_page_disabled":
+            register_page.className = "register_page";
+            break;
+        default:
+      }
 }
